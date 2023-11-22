5cd8e25ead4613127bf077c448d99d696ecee334
San4ik132 <99422931+San4ik132@users.noreply.github.com>
2023-11-22 19:28:49 +0400

    Add files via upload

---
+++ IB2/Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace IB2
+{
+    internal class Program
+    {
+
+        static void Percentages(string NameFile)
+        {
+            var FileContent = NameFile.ToUpper();
+            string Alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ ";
+            var LetterCount = new Dictionary<char, int>();
+
+            foreach (char c in FileContent)
+            {
+                char letter = c;
+                if (Alphabet.Contains(letter)) // Проверим, является ли символ буквой русского алфавита
+                {
+                    if (LetterCount.ContainsKey(letter))
+                    {
+                        LetterCount[letter]++;
+                    }
+                    else
+                    {
+                        LetterCount[letter] = 1;
+                    }
+                }
+            }
+
+            int totalLetters = LetterCount.Sum(x => x.Value); // Подсчитаем общее количество букв
+
+            var LetterPercentages = new Dictionary<char, double>();
+
+
+
+            foreach (var kvp in LetterCount)
+            {
+                LetterPercentages[kvp.Key] = Math.Round((kvp.Value / (double)totalLetters) * 100, 2); // Вычислим процентное соотношение для каждой буквы
+            }
+
+
+            var sortedLetterPercentages = LetterPercentages.OrderByDescending(x => x.Value);
+            foreach (var item in sortedLetterPercentages)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}%");
+            }
+            
+        }
+
+
+        static string DecryptionMain(string NameFile)
+        {
+            var FileContent = NameFile.ToUpper();
+            var K = new Dictionary<char, char>
+            {
+
+                { 'В', ' ' },
+                { 'Л', 'о' },
+                { 'Ц', 'н' },
+                { 'Ф', 'а' },
+                { 'А', 'и' },
+                { 'С', 'т' },
+                { 'И', 'д' },
+                { 'Ш', 'у' },
+                { 'Ы', 'к' },
+                { 'У', 'в' },
+                { 'Щ', 'г' },
+                { 'Е', 'э' },
+                { 'Г', 'е' },
+                { 'Э', 'ж' },
+                { 'Ъ', 'ы' },
+                { 'Ж', 'р' },
+                { 'Ч', 'з' },
+                { 'О', 'с' },
+                { 'Я', 'п' },
+                { 'Б', 'х' },
+                { 'Д', 'й' },
+                { 'Ь', 'л' },
+                { 'З', 'м' },
+                { 'Ю', 'ь' },
+                { 'Н', 'б' },
+                { ' ', 'ю' },
+                { 'К', 'я' },
+                { 'Т', 'ш' },
+                { 'Х', 'щ' },
+                { 'П', 'ч' },
+                { 'Й', 'ц' }
+
+            };
+            StringBuilder decryptedText = new StringBuilder();
+            foreach (var c in FileContent)
+            {
+                if (K.ContainsKey(c))
+                {
+                    decryptedText.Append(K[c]);
+                }
+                else
+                {
+                    decryptedText.Append(c);
+                }
+            }
+            return decryptedText.ToString();
+
+        }
+
+
+
+        static string DecryptionMain4(string NameFile)
+        {
+            var FileContent = NameFile.ToUpper();
+            var K = new Dictionary<char, char>
+            {
+                {'П', 'в'},
+                {'Й', 'е'},
+                {'С', 'д'},
+                {'Ж', 'н'},
+                {'Х', 'и'},
+                {'З', 'о'},
+                {'Б', 'с'},
+                {'Ш', 'ч'},
+                {'Д', 'л'},
+                {'Г', 'к'},
+                {'Р', 'г'},
+                {'Н', 'а'},
+                {'Е', 'м'},
+                {'И', 'п'},
+                {'Ф', 'ь'},
+                {'Ь', 'ю'},
+                {'В', 'т'},
+                {'А', 'р'},
+                {'О', 'б'},
+                {'У', 'ы'},
+                {'К', 'ж'},
+                {'Э', 'я'},
+                {'Ч', 'ц'},
+                {'М', 'у'},
+                {'Ы', 'э'},
+                {'Ц', 'й'},
+                {'Ъ', 'щ'},
+                {'Щ', 'ш'},
+                {'Л', 'з'},
+                {'Ю', 'ф'},
+                {'Я', 'х'},
+                {'Т', 'ъ'},
+
+
+            };
+
+            StringBuilder decryptedText = new StringBuilder();
+            foreach (var c in FileContent)
+            {
+                if (K.ContainsKey(c))
+                {
+                    decryptedText.Append(K[c]);
+                }
+                else
+                {
+                    decryptedText.Append(c);
+                }
+            }
+            return decryptedText.ToString();
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("---------------------- Таблица процентов первого текста ----------------------");
+            Program.Percentages(File.ReadAllText("Зашифрованный текст.txt"));
+            Console.WriteLine("---------------------- Расшифрованный первый текст ----------------------");
+            Console.WriteLine(Program.DecryptionMain(File.ReadAllText("Зашифрованный текст.txt")));
+
+
+
+
+            Console.WriteLine("--------------------- Таблица процентов текста варианта 4 ----------------------");
+            Program.Percentages(File.ReadAllText("input.txt"));
+            Console.WriteLine("--------------------- Расшифрованный текст Вариант 4 ----------------------");
+            Console.WriteLine(Program.DecryptionMain4(File.ReadAllText("input.txt")));
+
+        }
+    }
+}
---
+++ IB4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace IB4
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string inputText = File.ReadAllText("Input.txt");
+            string gamma = GenerateGamma(); // Генерируем гамму длиной 8 символов
+
+            string encryptedText = ED(inputText, gamma);
+            File.WriteAllText("Out.txt", encryptedText);
+            Console.WriteLine("Зашифрованный текст: " + encryptedText);
+
+            string encryptedText1 = File.ReadAllText("Out.txt");
+          
+            string decryptedText = ED(encryptedText1, gamma);
+            File.WriteAllText("Result.txt", decryptedText);
+            Console.WriteLine("Расшифрованный текст: " + decryptedText);
+        }
+        static string GenerateGamma()
+        {
+            string piDigits = "31415926"; // Первые 8 цифр числа π
+            string gamma = RepeatToLength(piDigits, 8);
+            return gamma;
+        }
+        static string RepeatToLength(string source, int length)
+        {
+            int sourceLength = source.Length;
+            return source.Substring(0, length % sourceLength) + source.Substring(0, length / sourceLength);
+        }
+        static string ED(string input, string gamma)
+        {
+            char[] inputArray = input.ToCharArray();
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                inputArray[i] = (char)(inputArray[i] ^ gamma[i % gamma.Length]); // Используем побитовое XOR для шифрования
+            }
+            return new string(inputArray);
+        }
+    }
+}
